cb3a761e73e49739be080d5beadaa6b2b625c74b
neoncd <neoncd@fixelsoft.com>
2021-08-15 14:30:01 +0900

    init2

---
+++ src/main/java/com/fix/Test01/Controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.fix.Test01.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Board")
+public class BoardController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "Board/list";
+    }
+}
