63ffae012611288070d4ec8e4965a26a7fc3707d
neoncd <neoncd@fixelsoft.com>
2021-08-15 14:33:54 +0900

    init2

--- src/main/java/com/fix/Test01/Controller/BoardController.java
+++ src/main/java/com/fix/Test01/Controller/BoardController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/Board")
+@RequestMapping("/board")
 public class BoardController {
 
     @GetMapping("/list")
-    public String list() {
-        return "Board/list";
+    public String mylist() {
+        return "board/list";
     }
 }
